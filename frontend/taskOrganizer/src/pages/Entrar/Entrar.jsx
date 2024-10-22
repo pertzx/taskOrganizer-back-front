1344b6dadb3340faf6f1e4eac632664481d888d8
Pyerre <pyerremarcio098@gmail.com>
2024-10-22 11:57:48 -0300

    atualizacao

--- frontend/taskOrganizer/src/pages/Entrar/Entrar.jsx
+++ frontend/taskOrganizer/src/pages/Entrar/Entrar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://task-organizer-back-front.vercel.app'; // URL do seu backend
+const API_URL = 'https://task-organizer-api.vercel.app'; // URL do seu backend
 
 function Entrar({ setPrecisoRegistrar, setLogado, setUsername, setDados }) {
   const [user, setUser] = useState('');
